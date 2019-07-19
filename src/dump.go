4b9a8a774f46cf16625ec21172b2ade9ab22dedb
Jack Baron <jackmbaron@gmail.com>
2019-07-19 03:19:03 +0100

    use global http client

--- src/dump.go
+++ src/dump.go
@@ -5,23 +5,16 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"time"
 )
 
 func readDump() ([]BeatmapInfo, error) {
-	client := &http.Client{
-		Timeout: time.Second * 5,
-	}
-
 	url := fmt.Sprintf("%v/api/download/dump/maps", beatSaverURL)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	userAgent := fmt.Sprintf("beatsaver-downloader/%v", sha1ver)
 	req.Header.Add("User-Agent", userAgent)
-
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 	"time"
 
@@ -12,10 +14,17 @@ var (
 	sha1ver = "unknown"
 	gitTag  string
 
+	client       *http.Client
+	userAgent    string
 	beatSaverURL = "https://beatsaver.com"
 )
 
 func main() {
+	userAgent = fmt.Sprintf("beatsaver-downloader/%v", sha1ver)
+	client = &http.Client{
+		Timeout: time.Second * 5,
+	}
+
 	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
 	s.Writer = os.Stderr
 	s.Suffix = " Downloading latest dump..."
